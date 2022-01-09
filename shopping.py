7e9cf8697c21a52fc0d8ca1b942916915257431f
s3926650 <95325733+s3926650@users.noreply.github.com>
2022-01-09 10:00:16 +0700

    Update shopping.py

--- shopping.py
+++ shopping.py
@@ -7,6 +7,7 @@
 # 6. Placing orders (the quantity must be updated when an item is bought)r
 from items import available_items, id_list  # import the items and its id
 from discount import discount  # import discount function from discount.py
+import json
 
 cart = []
 
@@ -193,6 +194,37 @@ def payment(cart):  # Feature 3
         return
     print("Welcome back")
     return
+
+
+def register():     #Feature 4
+    """Register personal information for new customers"""
+    print('Press "0" to escape. Press "1" after registration to continue'
+    go_register = 1
+    while(go_register != 0):
+        #Collect customer personal information
+        print("What's your name?")
+        name = input("Name: ")
+        print("How old are you?")
+        age = input("Age: ")
+        print("What's your phone number?")
+        phone = input("Phone number: ")
+        print("What's your email?")
+        email = input("Email: ")
+        print("What's your shipping address")
+        address = input("Shipping address: ")
+        with open('customer_information.txt') as file:
+            customer_info = json.load(file)
+        file.close()
+        #Adding customer personal information to the text file storing information
+        customer_info[name] = {'Age' : age,
+                               'Phone number' : phone,
+                               'Email' : email,
+                               'Shipping address' : address}
+        with open('information.txt', 'w') as file:
+            file.write(json.dumps(customer_info))
+        file.close()
+        print("You have successfully registered!")
+        go_register = int(input("Do you want to continue registering?"))
 
 
 def here():
